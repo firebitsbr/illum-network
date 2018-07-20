335e578b80fcd6b3e61295ae783cd2672ec0a679
Дукалис <odenscp@gmail.com>
2018-07-20 13:36:30 +0300

    Update servers.h

--- servers.h
+++ servers.h
@@ -2,6 +2,15 @@
 *	servers.h - Заголовочный файл для servers.c с перечнем
 *	всех функций и структур.
 *
+*	Пример инициализации модуля:
+*	illdb db;
+*	illsrv srv;
+*	illroute router;
+*	illsrv_init(&srv, &db, &router, file_stream);
+*
+*	Пример вызова функций:
+*	srv.start( some params.. );
+*
 *	@mrrva - 2018
 */
 #ifndef ILL_SERVERS
@@ -9,6 +18,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <pthread.h>
 #include <unistd.h>
@@ -18,7 +28,6 @@
 #include <errno.h>
 #include "./database.h"
 #include "./router.h"
-#define TEXTSIZE_BUFER 2000
 #define MAXTEXTSIZE 9000
 #define SERVER_TIMEOUT 10
 #define ILLUM_PORT 110
