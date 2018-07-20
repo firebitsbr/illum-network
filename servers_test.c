f8c5f6a47c15013e7a13ab5c200b0eaca36e3ced
Дукалис <odenscp@gmail.com>
2018-07-20 13:33:17 +0300

    Update servers_test.c

--- servers_test.c
+++ servers_test.c
@@ -4,8 +4,10 @@
 int main()
 {
 	illdb db;
+	illroute router;
 	illsrv srv;
 	FILE *fp = fopen("./log.txt", "a+");
+	struct threads thrds;
 
 	/* Init illdb */
 	if (!illdb_init("./illum.db", &db, fp)) {
@@ -14,13 +16,14 @@ int main()
 	}
 
 	/* Init illsrv */
-	if (!illsrv_init(&srv, &db, fp)) {
+	if (!illsrv_init(&srv, &db, &router, fp)) {
 		printf("Error: Can't init illsrv struct.\n");
 		return 1;
 	}
-	pthread_join(srv.getserver, NULL);
-	pthread_join(srv.sendserver, NULL);
 
+	thrds = srv.start(srv);
+
+	pthread_join(thrds.server, 0);
 
 	return 0;
 }
