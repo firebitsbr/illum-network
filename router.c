d37b1db638c016b7c95c3828bbf34c3b40450e62
Дукалис <odenscp@gmail.com>
2018-08-14 23:08:32 +0300

    Update router.c

--- router.c
+++ router.c
@@ -13,16 +13,11 @@ struct onion {
 
 };
 
-struct enchdr {
+struct headers {
 	unsigned int type, nodesnum;
 	char *ipaddr, *hash;
 	struct onion onion;
 	bool is_onion;
-};
-
-struct clearhdr {
-	unsigned int type, nodesnum;
-	char *ipaddr, *hash, *cert;
 };
 
 struct functions {
@@ -34,7 +29,7 @@ struct functions {
 */
 /*static bool illrouter_mkclearhdr(struct node_list *, unsigned int,
 	char *, char *, enum illheader);*/
-static bool illrouter_clearmsg(json_object *, struct clearhdr *);
+static bool illrouter_decode(json_object *, struct headers *);
 static void illrouter_newroute(enum illheader, char *);
 static void illrouter_befriends(json_object *, char *);
 static void illrouter_straight(json_object *, char *);
@@ -120,57 +115,56 @@ exit_create:
 *	illrouter_newroute - Функция создания нового маршрута.
 *
 *	@type - Тип маршрута.
+*	@ipaddr - Адрес для отправки.
 */
 static void illrouter_newroute(enum illheader type, char *ipaddr)
 {
 	unsigned int nodenum = 0;
 	json_object *jobj;
-	char *json;
+	char *json, *hash;
 
-	if (!ipaddr) {
-		fprintf(errfile, "Error: Incorrect ipaddr.\n");
+	if (!ipaddr || ipaddr == NULL || strlen(ipaddr) < 7) {
+		fprintf(errfile, "Error: Incorrect ipaddr in newroute.\n");
 		return;
 	}
 
 	jobj = json_object_new_object();
 	nodenum = db->nodenum(errfile);
+	hash = "hghghghghghghghg";
 
 	json_object_object_add(jobj, "nodenum", json_object_new_int(nodenum));
-	json_object_object_add(jobj, "type", json_object_new_int((int)type));
 	json_object_object_add(jobj, "ipaddr", json_object_new_string(""));
+	json_object_object_add(jobj, "hash", json_object_new_string(hash));
+	json_object_object_add(jobj, "type", json_object_new_int(type));
 
-	if (type == ILL_BEFRIENDS) {
-		/*
-			Getting hash and cert.
-		*/
-		json_object_object_add(jobj, "hash", json_object_new_string("f"));
-		json_object_object_add(jobj, "cert", json_object_new_string("f"));
+	if (type == ILL_ONION) {
+
 	}
 
-	json = json_object_to_json_string(jobj);
+	json = (char *)json_object_to_json_string(jobj);
+	db->newtask(ipaddr, NULL, json, errfile);
 	json_object_put(jobj);
-
-
 }
 /**
-*	illrouter_clearmsg - Функция считывания json елементов.
+*	illrouter_decode - Функция считывания json елементов.
 *
 *	@jobj - Json объект маршрута.
 *	@msg - Структура заголовков.
 */
-static bool illrouter_clearmsg(json_object *jobj,
-	struct clearhdr *msg)
+static bool illrouter_decode(json_object *jobj, struct headers *msg)
 {
 	bool status = false;
 
 	if (!jobj || !msg || msg == NULL) {
-		fprintf(errfile, "Error: Invalid params in clearmsg\n");
+		fprintf(errfile, "Error: Invalid params in clearmsg.\n");
 		return status;
 	}
 
+	msg->is_onion = false;
+
 	json_object_object_foreach (jobj, key, value) {
 		if (strlen(key) < 2 || strlen(key) > 15) {
-			fprintf(errfile, "Warring: Can't decode json(2)\n");
+			fprintf(errfile, "Warring: Can't decode json(2).\n");
 			return status;
 		}
 
@@ -183,14 +177,16 @@ static bool illrouter_clearmsg(json_object *jobj,
 			msg->nodesnum = (int)json_object_get_int(value);
 		else if (strcmp(key, "hash") == 0)
 			msg->hash = (char *)json_object_get_string(value);
-		else if (strcmp(key, "cert") == 0)
-			msg->cert = (char *)json_object_get_string(value);
 		else if (strcmp(key, "type") == 0)
 			msg->type = (int)json_object_get_int(value);
+		else if (strcmp(key, "onion") == 0) {
+			msg->is_onion = true;
+			// Decoding onion struct
+		}
 	}
 
-	if (msg->ipaddr && msg->cert && msg->hash
-		&& msg->type >= 0 && msg->nodesnum >= 0)
+	if (msg->ipaddr && msg->hash && msg->type >= 0
+		&& msg->nodesnum >= 0)
 		status = true;
 
 	return status;
@@ -203,16 +199,16 @@ static bool illrouter_clearmsg(json_object *jobj,
 */
 static void illrouter_newnode(json_object *jobj, char *ipaddr)
 {
-	struct node_list *list, own;
-	struct clearhdr msg;
+	struct node_list *list;
+	char *headers, *ipc;
+	struct headers msg;
 	unsigned int len;
-	char *headers;
 
 	if (!jobj || jobj == NULL || !ipaddr || ipaddr == NULL) {
 		fprintf(errfile, "Warring: Invalid json object(1).\n");
 		return;
 	}
-	if (!illrouter_clearmsg(jobj, &msg)) {
+	if (!illrouter_decode(jobj, &msg)) {
 		fprintf(errfile, "Warring: Can't decode json(3).\n");
 		return;
 	}
@@ -223,22 +219,35 @@ static void illrouter_newnode(json_object *jobj, char *ipaddr)
 		return;
 	}
 	else if (len < MAXNODES) {
-	
-		//illrouter_resend(&own, 1, /*Here BEFRIENDS*/);
+		ipc = (msg.ipaddr && strlen(msg.ipaddr) >= 7)
+			? msg.ipaddr : ipaddr;
+
+		db->newnode(ipc, msg.hash, 10, errfile);
+		illrouter_newroute(ILL_BEFRIENDS, ipc);
 	}
 
 	if (!msg.ipaddr || msg.ipaddr == NULL || strlen(msg.ipaddr) < 7)
-		json_object_object_add(jobj, "ipaddr", json_object_new_string(ipaddr));
+		json_object_object_add(jobj, "ipaddr", 
+								json_object_new_string(ipaddr));
 
 	if (!(headers = (char *)json_object_to_json_string(jobj))) {
 		fprintf(errfile, "Error: Invalid json string in "
 				"resend.\n");
-		return;
+		goto exit_newnode;
 	}
 
 	for (int i = 0; i < len; i++)
-		if (list[i].ipaddr && strlen(list[i].ipaddr) >= 7)
-			db->newtask(list[i].ipaddr, NULL, NULL, headers, errfile);
+		if (list[i].ipaddr && strlen(list[i].ipaddr) >= 7) {
+			db->newtask(list[i].ipaddr, NULL, headers, errfile);
+			free(list[i].ipaddr);
+			free(list[i].hash);
+		}
+
+exit_newnode:
+	if (headers && headers != NULL)
+		free(headers);
+	if (list && list != NULL)
+		free(list);
 }
 /**
 *	illrouter_newnode - Функция создания маршрута для
@@ -248,14 +257,14 @@ static void illrouter_newnode(json_object *jobj, char *ipaddr)
 */
 static void illrouter_ping(json_object *jobj, char *ipaddr)
 {
-	struct clearhdr msg;
+	struct headers msg;
 	
 	if (!jobj || jobj == NULL) {
 		fprintf(errfile, "Error: Invalid json object(2).\n");
 		return;
 	}
 
-	illrouter_clearmsg(jobj, &msg);
+	illrouter_decode(jobj, &msg);
 }
 /**
 *	illrouter_newnode - Функция создания маршрута для
@@ -265,14 +274,14 @@ static void illrouter_ping(json_object *jobj, char *ipaddr)
 */
 static void illrouter_stat(json_object *jobj, char *ipaddr)
 {
-	struct clearhdr msg;
+	struct headers msg;
 	
 	if (!jobj || jobj == NULL) {
 		fprintf(errfile, "Error: Invalid json object(3).\n");
 		return;
 	}
 
-	illrouter_clearmsg(jobj, &msg);
+	illrouter_decode(jobj, &msg);
 }
 /**
 *	illrouter_befriends - Функция создания маршрута для
@@ -282,14 +291,14 @@ static void illrouter_stat(json_object *jobj, char *ipaddr)
 */
 static void illrouter_befriends(json_object *jobj, char *ipaddr)
 {
-	struct clearhdr msg;
+	struct headers msg;
 	
 	if (!jobj || jobj == NULL) {
 		fprintf(errfile, "Error: Invalid json object(4).\n");
 		return;
 	}
 
-	illrouter_clearmsg(jobj, &msg);
+	illrouter_decode(jobj, &msg);
 }
 /**
 *	illrouter_newnode - Функция создания маршрута для
@@ -311,85 +320,3 @@ static void illrouter_onion(json_object *jobj, char *ipaddr)
 {
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* FUNCTIONS, WHICH WILL BE USED IN THE FUTURE */
-
-
-/**
-*	illrouter_mkclearhdr - Функция создания заголовков
-*	незашифрованных сообщений.
-*
-*	@list - Список получателей.
-*	@len - Количество получателей.
-*	@ipaddr - Ip отправителя
-*	@hash - Хэш отправителя.
-*	@type - Тип сообщения.
-
-static bool illrouter_mkclearhdr(struct node_list *list,
-	unsigned int len, char *ipaddr, char *hash,
-	enum illheader type)
-{
-
-	if (!list || list == NULL || len < 1 || !ipaddr || strlen(ipaddr) < 7
-		|| !hash || strlen(hash) < 10) {
-		fprintf(errfile, "Error: Incorrect input data in mkclearhdr\n");
-		return false;
-	}
-	if (type != ILL_NEWNODE && type != ILL_PING && type != ILL_STAT
-		&& type != ILL_BEFRIENDS) {
-		fprintf(errfile, "Error: Incorrect type of message.\n");
-		return false;
-	}
-
-	for (int i = 0; i < len; i++) {
-		if (!list[i].ipaddr || !list[i].hash || strlen(list[i].ipaddr) < 7
-			|| strlen(list[i].hash) < 10) {
-			fprintf(errfile, "Error: Input value is incorrect in mkclearhdr\n");
-		}
-
-		json_object *jobj = json_object_new_object();
-
-		json_object_object_add(jobj, "type", json_object_new_int(type));
-		json_object_object_add(jobj, "", json_object_new_string());
-
-		json_object_put(jobj);
-	}
-
-
-	jobj = json_object_new_object();
-	
-		json_object *node = json_object_new_object(), *main = json_object_new_object();
-	// Создаем json массив
-	json_object_object_add(node, "hash", json_object_new_string(illum->user->certhash));
-	json_object_object_add(node, "public_key", json_object_new_string(illum->user->public_cert));
-	json_object_object_add(main, "node", node);
-	json_object_object_add(main, "type", json_object_new_int(type));
-	json_object_object_add(main, "text", json_object_new_string(text));
-	// Возвращаем json строку
-	return json_object_to_json_string(main);
-	
-
-}*/
