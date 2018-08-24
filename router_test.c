4190c6fe1221c784193d36b5751b21d7ee8a3879
Дукалис <odenscp@gmail.com>
2018-08-24 16:44:07 +0300

    Update router_test.c

--- router_test.c
+++ router_test.c
@@ -4,22 +4,40 @@
 int main() 
 {
 	illdb db;
+	illenc enc;
 	illrouter rte;
 	FILE *fp = fopen("./log.txt", "a+");
-	
+
 	/* Init illdb */
 	if (!illdb_init("./illum.db", &db, fp)) {
 		printf("Error: Can't init illdb struct.\n");
 		return 1;
 	}
 
-	if (!illrouter_init(&rte, &db, fp)) {
+	if (!illenc_init(&enc, &db, fp)) {
+		printf("Error: Can't init illenc struct.\n");
+		return 1;
+	}
+
+	if (!illrouter_init(&rte, &db, &enc, fp)) {
 		printf("Error: Can't init illrouter struct.\n");
 		return 1;
 	}
 
+	rte.read("{\"type\" : 1, \"nodenum\" : 0, \"ipaddr\" : \"\", "
+		"\"hash\" : \"bvmnbvmnbvmnvbmnbvvbmn\"}", "192.168.1.1");
+	/*
+	> NEW NODE + BE FRIENDS
+
 	rte.read("{\"type\" : 0, \"nodenum\" : 0, \"ipaddr\" : \"\", "
-		"\"hash\" : \"dfghdfhdfhhfdhfdfdfdh\"}", "192.168.1.1");
+		"\"hash\" : \"bvmnbvmnbvmnvbmnbvvbmn\"}", "192.168.1.1");
+
+	rte.read("{\"type\" : 3, \"nodenum\" : 0, \"ipaddr\" : \"\", "
+		"\"hash\" : \"bvmnbvmnbvmnvbmnbvvbmn\"}", "192.168.1.1");
+
+	rte.read("{\"type\" : 3, \"nodenum\" : 0, \"ipaddr\" : \"\", "
+		"\"hash\" : \"bvmnbvmnbvmnvbmnbvvbmn\"}", "192.168.1.1");
+	*/
 
 	return 0;
 }
